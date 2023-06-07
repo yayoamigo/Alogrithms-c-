51454b421b2a7a03990e143fd25b238ddc71df47
yayoamigo <david._101@hotmail.com>
2023-06-07 07:14:07 -0500

    "add warriors battle"

---
+++ warrios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using warrios;
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Warrior thor = new Warrior("Thor", 1000, 120, 40);
+            MagicWarriors loki = new MagicWarriors("Loki", 800, 100, 40, 60);
+
+            Battle.StartFight(thor, loki);
+
+
+        }
+    }
+
---
+++ warrios/Warriors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace warrios
+{
+    class Warrior
+    {
+        public string name;
+        public int health;
+        public int attackMax;
+        public int blockMax;
+        Random rnd = new Random();
+        public Warrior(string name = "warrior", int health = 0, int attackMax = 0, int blockMax = 0)
+        {
+            this.name = name;
+            this.health = health;
+            this.attackMax = attackMax;
+            this.blockMax = blockMax;
+        }
+        public int Attack()
+        {
+            return rnd.Next(1, this.attackMax);
+        }
+        public virtual int Block()
+        {
+            return rnd.Next(1, this.blockMax);
+        }
+    }
+}
