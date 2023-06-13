99744d4d0cd8e563b9b871830454e5570c692c15
yayoamigo <david._101@hotmail.com>
2023-06-13 15:01:40 -0500

    Duplicate

---
+++ ContainsDup/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ContainsDup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    return true;
+                } else
+                {
+                    dic.Add(num, num);
+                }
+            } return false;
+        }
+    }
+}
