cdedf496ca7aad6f409dde2f71418ab7633400c8
yayoamigo <david._101@hotmail.com>
2023-06-15 11:04:05 -0500

    solved

---
+++ StockBest/StockBest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StockBest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int max = 0;
+            int min = prices[0];
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                }
+
+                else if ((prices[i] - min) > max)
+                {
+                    max = prices[i] - min;
+                }
+            }
+            return max;
+        }
+    }
+}
+
