b3c6b966d22538380cb58a473b3fa292a00fe4e0
yayoamigo <david._101@hotmail.com>
2023-06-23 14:19:41 -0500

    solved

---
+++ MaxWater/MaxWater/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MaxWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Solution solution = new Solution();
+            Console.Write(solution.MaxArea(arr));
+        }
+        public class Solution
+        {
+            public int MaxArea(int[] height)
+            {
+                int i = 0;
+                int j = height.Length - 1;
+                int max = 0;
+                int iHeight;
+                int jHeight;
+                int temp;
+
+                while (j > i)
+                {
+                    iHeight = height[i];
+                    jHeight = height[j];
+
+
+                    if (iHeight > jHeight)
+                    {
+                        temp = jHeight * (j - i);
+                        j--;
+                    }
+                    else
+                    {
+                        temp = iHeight * (j - i);
+                        i++;
+                    }
+
+                    if (temp > max)
+                        max = temp;
+
+                }
+                return max;
+            }
+        }
+    }
+}
