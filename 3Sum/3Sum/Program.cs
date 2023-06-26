fc23f1f423e3dcd897a47eda9afba19807ca6a2e
yayoamigo <david._101@hotmail.com>
2023-06-26 13:49:19 -0500

    3sum

---
+++ 3Sum/3Sum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[-1,0,1,2,-1,-4]
+
+        }
+    }
+    public class Solution
+    {
+        public List<IList<int>> ThreeSum(int[] nums)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+
+            // Sort the array to handle duplicate triplets
+            Array.Sort(nums);
+            [-4, -1, -1, 0, 1, 2]
+        for (int i = 0; i < nums.Length - 2; i++)
+            {
+                // Skip duplicates for i
+                if (i > 0 && nums[i] == nums[i - 1])
+                    continue;
+
+                int left = i + 1;
+                int right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if (sum == 0)
+                    {
+                        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                        // Skip duplicates for left and right
+                        while (left < right && nums[left] == nums[left + 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right - 1])
+                            right--;
+
+                        left++;
+                        right--;
+                    }
+                    else if (sum < 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+
+}
