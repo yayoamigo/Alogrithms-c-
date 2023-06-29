69617075fcf8ac30a3089195d0a0ac36627aeec6
yayoamigo <david._101@hotmail.com>
2023-06-29 12:40:56 -0500

    csv

---
+++ CSV/CSV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CSVReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = @"C:\Users\david\Desktop\directory\addresses.csv";
+            string[] lines = File.ReadAllLines(path);
+
+            List<string> names = new List<string>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] name = lines[i].Split(',');
+                names.Add(name[0]);
+            }
+
+            Console.WriteLine(names[1]);
+        }
+    }
+}
+
